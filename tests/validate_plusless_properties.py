379062948b282e1a09b101ac46d27eace1ab6dd8
Evgeniy Bobok <evgeniy.bobok@traderepublic.com>
2023-10-04 22:55:57 +0200

    Added group defs and fixed tests for model_configs

--- tests/validate_plusless_properties.py
+++ tests/validate_plusless_properties.py
@@ -2,17 +2,16 @@ import json
 from jsondiff import diff
 
 PROJECT_SCHEMA_FILES = [
-    "./schemas/dbt_project.json",
-    "./schemas/1.5/dbt_project-1.5.json",
-    "./schemas/1.6/dbt_project-1.6.json"
+    "../schemas/dbt_project.json",
+    "../schemas/1.5/dbt_project-1.5.json",
+    "../schemas/1.6/dbt_project-1.6.json"
 ]
 
 def check_equivalency(key, node_type, node_properties):
     if not key.startswith("+"):
         counterpart_key = "+" + key
     else:
-        counterpart_key = key[1:]
-
+        counterpart_key = key
     if counterpart_key not in node_properties.keys():
         raise Exception(f"{key} doesn't have an equivalent {counterpart_key} defined")
     key_properties = node_properties[key]
@@ -26,9 +25,9 @@ node_types = ['model_configs', 'seed_configs', 'snapshot_configs', 'source_confi
 for filepath in PROJECT_SCHEMA_FILES:
     with open(filepath, "r") as f:
         data = json.load(f)
-        for node_type in node_types:
+        for node_type in node_types[:1]:
             node_properties = data["$defs"][node_type]["properties"]
             for key in node_properties.keys():
                 check_equivalency(key, node_type, node_properties)
-
-
+            print(f"{filepath} {node_type} tests passed")
+            
