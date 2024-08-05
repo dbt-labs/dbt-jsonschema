8ea4b7772ae8b0089d6fed83934bfa4acdb5f853
Dave Connors <dave.connors@fishtownanalytics.com>
2024-08-05 17:29:34 -0500

    update script, and generate pydantic for all latest files

--- src/generate.py
+++ src/generate.py
@@ -1,19 +1,29 @@
 import json
 import os
 
+from latest.dbt_cloud import DbtCloud
+from latest.dbt_project import DbtProject
 from latest.dbt_yml_files import DbtYmlFiles
+from latest.dependencies import Dependencies, Packages
+from latest.selectors import Selectors
 
 if __name__ == "__main__":
-    versions = ["latest"]
-    files = {"dbt_yml_files": DbtYmlFiles}
-    for v in versions:
-        output_directory = os.path.join(
-            os.path.dirname(__file__), "../", f"schemas/{v}-generated"
+    files = {
+        "dbt_yml_files": DbtYmlFiles, 
+        "dependencies": Dependencies, 
+        "packages": Packages,
+        "selectors": Selectors,
+        "dbt_project": DbtProject,
+        "dbt_cloud": DbtCloud,
+
+    }
+    output_directory = os.path.join(
+        os.path.dirname(__file__), "../", f"schemas/latest-generated"
+    )
+    for file_name, model in files.items():
+        file_path = os.path.join(
+            output_directory, f"{file_name}-latest-generated.json"
         )
-        for file_name, model in files.items():
-            file_path = os.path.join(
-                output_directory, f"{file_name}-{v}-generated.json"
-            )
-            schema = model.model_json_schema(mode="validation")
-            with open(file_path, "w") as file:
-                json.dump(schema, file, indent=2)
+        schema = model.model_json_schema(mode="validation")
+        with open(file_path, "w") as file:
+            json.dump(schema, file, indent=2)
